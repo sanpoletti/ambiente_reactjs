6d45dcc873be7851fd5af773c65654122520f13f
Sandra Poletti <sanpoletti@gmail.com>
2023-02-26 18:00:53 -0300

    created QA env

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // ambiente produccion
+  // ambiente QA
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Produccion</h1>
+        <h1>QA</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
